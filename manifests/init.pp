2837c4ec0e5e5ab89ef3d349e23ec63048c8c49d
Jeremy Adams <jeremy@puppetlabs.com>
2014-11-28 14:58:32 -0800

    Adds options for puppet-lint to vimrc through param

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,8 @@
 # init.pp
-class puppet_vim_env ( $homedir = $::homedir ) {
+class puppet_vim_env (
+  $homedir = $::homedir,
+  $puppetlint_opts = '--no-documentation-check --no-80chars-check --no-autoloader_layout-check'
+) {
 
   file { "${homedir}/.vim":
     ensure => directory,
--- manifests/pathogen.pp
+++ manifests/pathogen.pp
@@ -19,5 +19,13 @@ class puppet_vim_env::pathogen (
     ensure => file,
     source => 'puppet:///modules/puppet_vim_env/vimrc',
   }
+  
+  if $puppet_vim_env::puppetlint_opts != '' {
+    file_line { 'add puppet-lint options to vimrc':
+      path => "${homedir}/.vimrc",
+      line => "let g:syntastic_puppet_puppetlint_args=\'${puppet_vim_env::puppetlint_opts}\'",
+    }
+  }
+
 }
 
